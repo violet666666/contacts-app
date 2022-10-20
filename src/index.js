30a9781ddde685224f31db0d852ab3dd13a3a797
violet666666 <setanhantu11111@gmail.com>
2022-10-20 23:44:51 +0700

    edit images folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-const element = <h1>Hello, world!</h1>;
-
-const root = createRoot(document.getElementById("root"));
-root.render(element);
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ContactApp from './ContactApp';
+ 
+ 
+const root = createRoot(document.getElementById('root'));
+root.render(<ContactApp />);
